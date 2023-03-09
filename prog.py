dd7fc1189d533a0ef5747ca22309817243a6f26c
Tommaso Fonda <fondatommaso2@gmail.com>
2023-03-09 17:37:05 +0100

    Place snakes on the grid naively

--- prog.py
+++ prog.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import sys
+from Snake import Snake
 
 def parse_input(filename):
 	matrix = np.loadtxt(filename, skiprows=2)
@@ -14,5 +15,16 @@ if __name__ == "__main__":
 		filename = str(sys.argv[1])
 	parse_input(filename)
 	with open(filename, "r") as fp:
-		snakes = np.array(fp.readlines()[1].split()).astype(np.int64)
-	print(snakes)
+		snakes_data = np.array(fp.readlines()[1].split()).astype(np.int64)
+	print(snakes_data)
+	print("\n\n\n")
+
+	snakes = [Snake(length) for length in snakes_data]
+	line = 0
+	for snake in snakes:
+		snake.place(line, 0)
+		line += 1
+
+	for snake in snakes:
+		print(snake)
+		print(snake.output())
